f0493c8013241548fd58c3d90331a59fb6b854e3
bemnetzadmassu-tech <bemnetzadmassu@gmail.com>
2026-07-01 00:53:19 +0300

    Disable SSL for Neon connection

--- api/_lib/db.js
+++ api/_lib/db.js
@@ -2,8 +2,10 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
+    ssl: false,          // ← CHANGE: disable SSL
     max: 10,
 });
+
+module.exports = pool;
 
 module.exports = pool;
